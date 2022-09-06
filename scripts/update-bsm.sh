31b13dea3c423fa0ca201f5a4b27c82ea87b3a33
Tomer Gabel <tomer@tomergabel.com>
2022-09-06 16:10:05 +0300

    Improved update script for Barry Service Manager

--- scripts/update-bsm.sh
+++ scripts/update-bsm.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cd /opt/barry-service-manager
-
-git fetch
-
-git reset --hard origin/master
-
-servicectl restart barry-service-manager
+su barry -c 'git fetch && git reset --hard origin/master'
+systemctl restart barry-service-manager.service
