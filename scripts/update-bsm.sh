c9f140625817a79765436820c175f57d3781e59c
Tomer Gabel <tomer@tomergabel.com>
2022-09-06 16:14:24 +0300

    Update sudo permissions on BSM update

--- scripts/update-bsm.sh
+++ scripts/update-bsm.sh
@@ -5,3 +5,10 @@ set -e
 cd /opt/barry-service-manager
 su barry -c 'git fetch && git reset --hard origin/master'
 systemctl restart barry-service-manager.service
+
+# Update script sudo permissions (in case of new/removed scripts)
+{
+  for script in /opt/barry-service-manager/scripts/*.sh; do
+    echo "barry ALL= NOPASSWD: $script"
+  done
+} >/etc/sudoers.d/barry-app-ctrl
