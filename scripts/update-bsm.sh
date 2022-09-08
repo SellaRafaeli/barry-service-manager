5064904fa7ef1257af93f91d037f8f2cfcd34f54
Tomer Gabel <tomer@tomergabel.com>
2022-09-08 16:16:15 +0300

    Update systemd unit on BSM update

--- scripts/update-bsm.sh
+++ scripts/update-bsm.sh
@@ -5,7 +5,26 @@ set -e
 cd /opt/barry-service-manager
 su barry -c 'git fetch && git reset --hard origin/master'
 bundle install
-systemctl restart barry-service-manager.service
+
+# Rewrite the systemd unit (in case of new/modified environment)
+cat >/etc/systemd/system/barry-service-manager.service <<-EOF
+	[Unit]
+	Description=Barry Service Manager
+	After=network.target
+
+	[Service]
+	Type=simple
+	WorkingDirectory=/opt/barry-service-manager
+	User=barry
+	ExecStart=bundle exec rackup -p 8100 -o 0.0.0.0
+	Restart=on-failure
+	SyslogIdentifier=barry-service-manager
+	Environment=MONGODB_CONN_TEMPLATE=${MONGODB_CONN_TEMPLATE}
+	Environment=TOKEN=${TOKEN}
+
+	[Install]
+	WantedBy=multi-user.target
+EOF
 
 # Update script sudo permissions (in case of new/removed scripts)
 {
@@ -13,3 +32,6 @@ systemctl restart barry-service-manager.service
     echo "barry ALL= NOPASSWD:SETENV: $script"
   done
 } >/etc/sudoers.d/barry-app-ctrl
+
+systemctl daemon-reload
+systemctl restart barry-service-manager.service
